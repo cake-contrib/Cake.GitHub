5e7f8dd52eb235561c770894ab2993cfb14e3208
Geert van Horrik <geert@catenalogic.com>
2024-08-12 13:26:07 +0200

    Update

--- src/Cake.GitHub/Properties/AssemblyInfo.cs
+++ src/Cake.GitHub/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿#pragma warning disable IDE0005
+using System.Reflection; // Required by cake attributes
+#pragma warning restore IDE0005
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Cake.GitHub.Tests")]
