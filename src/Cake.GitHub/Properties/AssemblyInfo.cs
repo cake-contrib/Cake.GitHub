f0b927e028fcc3e63eccbc8d5a145a84e286b084
Geert van Horrik <geert@catenalogic.com>
2024-08-12 13:25:09 +0200

    Fix duplicate attribute issue during build

--- src/Cake.GitHub/Properties/AssemblyInfo.cs
+++ src/Cake.GitHub/Properties/AssemblyInfo.cs
@@ -4,5 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Cake.GitHub.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
-[assembly: AssemblyVersion("1.0.0")]
 // Note: other attributes are set by Cake
